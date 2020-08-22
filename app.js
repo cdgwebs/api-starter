14ce379a748468269960ce15917641b2e7ff428f
David M Graham <d5graha@gmail.com>
2020-08-21 22:21:33 -0500

    Working with process.env.<ConfigKey>

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 require('dotenv').config();
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const cookieSession = require('cookie-session');
 
+const app = express();
+
+const port = 3000;
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -29,6 +32,13 @@ app.use(function(req, res, next) {
 
 // Cookie Store in Session
 app.set('trust proxy', 1);
+/* // Can be used for storing session variables
+    if (!request.session.visitCount) {
+        request.session.visitCount = 0;
+    }
+    request.session.visitCount += 1;
+    console.log(`Visit count is: ${request.session.visitCount}`);
+*/
 app.use(
     cookieSession({
         name: 'session',
@@ -51,6 +61,9 @@ app.listen(3000, () => {
     // Just to log the root path
     console.log(`Home directory: ${path.join(__dirname, './static')}`);
     console.log(`Express server listening on port ${port}`);
+    console.log(`Production Mongo Connection ${process.env.PRODUCTION_DB_DSN}`);
+    console.log(`Development Mongo Connection ${process.env.DEVELOPMENT_DB_DSN}`);
+    console.log(`Test Mongo Connection ${process.env.TEST_DB_DSN}`);
 });
 
 module.exports = () => {
