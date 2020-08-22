0e662061462c11ce6ba8bc8fc39befc901d9c0a2
David M Graham <d5graha@gmail.com>
2020-08-21 22:00:38 -0500

    Doesn't work yet

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+require('dotenv').config();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -24,18 +24,35 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
+
+// Cookie Store in Session
+app.set('trust proxy', 1);
+app.use(
+    cookieSession({
+        name: 'session',
+        keys: ['ofhjadsdgoihaig', 'asdkfhaddoifh'],
+    })
+);
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
-module.exports = app;
+app.listen(3000, () => {
+    // Just to log the root path
+    console.log(`Home directory: ${path.join(__dirname, './static')}`);
+    console.log(`Express server listening on port ${port}`);
+});
+
+module.exports = () => {
+    return app;
+};
